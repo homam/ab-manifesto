5349e8ef2f921cecbd138395d62c833d7dc74cc5
Homam <bluesnowball@gmail.com>
2014-08-23 18:05:05 +0400

    build gh-pages script

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -6,11 +6,5 @@ lsc build-gh-pages.ls > temp/index.html
 ./build.sh
 cp views/home/javascript/*.js temp/home/javascript/
 cp views/home/style/*.css temp/home/style/
-
-# find views/*/*/*.(css|js) | while read $file
-# do
-#     echo "Moving $file"
-#     #cp "$file" "${file/static/changethis}.xml"
-# done
-
-# find views/*/*/*.(css|js) -exec echo {} \; -exec cp {} temp/{} \;
+git checkout gh-pages
+mv temp/* . 
